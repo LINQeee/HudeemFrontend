c70ac6d104954ae1d84e9a51f9ee5a8cdc6a87e9
Adrian <adrianvved@gmail.com>
2023-12-02 22:48:37 +0300

    Refactor build configuration for outDir and add base path in vite.config.ts and tsconfig.json.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HudeemFrontend/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3001
+  },
+  build: {
+    outDir: 'dist',
   },
 })
