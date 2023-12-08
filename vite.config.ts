8e697ebe04ed87df8ad83a13c52e861ecaab02d9
Adrian <adrianvved@gmail.com>
2023-12-08 18:43:53 +0300

    Cache policy settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,23 @@ import {viteStaticCopy} from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      viteStaticCopy({
-        targets: [{
-            src: "CNAME",
-            dest: "."
-        }]
-      })
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3001
-  },
-  build: {
-    outDir: 'dist',
-  },
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [{
+                src: "githubConfigs/CNAME",
+                dest: "."
+            }, {
+                src: "githubConfigs/.htaccess",
+                dest: "."
+            }]
+        })
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 3001
+    },
+    build: {
+        outDir: 'dist',
+    },
 })
